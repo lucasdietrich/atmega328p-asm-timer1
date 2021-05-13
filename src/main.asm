575b6521d7d5a0e9a1110140f8ab7b894eb9e681
Adecy <ld.adecy@gmail.com>
2021-05-13 21:14:24 +0200

    fixed typos

--- src/main.asm
+++ src/main.asm
@@ -3,23 +3,23 @@
 ; equivalent to 1 second with clock select prescaler = 1024
 ; 15625 * 1024 / 16MHz = 1s
 ; 0xFFFFu - 15625u = 0xc2f6 
-#define TIMER_COUNT_1S_H 0xFFFFu - 15625u
+#define TIMER_COUNT_1S 0xFFFFu - 15625u
 #define TIMER_COUNT_1S_H 0xc2
 #define TIMER_COUNT_1S_L 0xf6
 
-// in this script we make sure that every register is used only one time 
-// because with dont push/pop registers
+; in this script we make sure that every register is used only one time 
+; because with dont push/pop registers
 
-// we assume TIMER1_OVF_vect is the only function executed (no need to pusH/pop register to/from stack)
+; we assume TIMER1_OVF_vect is the only function executed (no need to pusH/pop register to/from stack)
 
-// export functions
+; export functions
 .global main
 .global TIMER1_OVF_vect // if __vector_13 does not appear in __vectors table, declare assoc *_OVF_vect .global
 
 TIMER1_OVF_vect:
     call ResetTimer1
 
-    // toggle led
+    ; toggle led
     in r17, _SFR_IO_ADDR(PORTB)
 	eor r17, r16
 	out _SFR_IO_ADDR(PORTB), r17	
